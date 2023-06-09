3f3352be1b57da55964274d4246cb1a787d07d38
plla2 <jhbljs92@naver.com>
2023-06-09 19:50:08 +0900

    Add todo, Add Input styling

--- todo-fcc/src/App.tsx
+++ todo-fcc/src/App.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import "./App.scss";
 import InputField from "./components/InputField";
+import { Todo } from "./model";
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
   return (
     <div className="App">
       <span className="heading">Todo App</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
 };
--- todo-fcc/src/components/InputField.tsx
+++ todo-fcc/src/components/InputField.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import "./InputField.scss";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
   return (
-    <form className="input" action="">
-      <input type="input" placeholder="Enter a task" className="input__box" />
+    <form className="input" onSubmit={handleAdd}>
+      <input
+        type="input"
+        placeholder="할일을 입력하세요."
+        className="input__box"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button className="input__submit" type="submit">
+        등록
+      </button>
     </form>
   );
 };
